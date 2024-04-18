2af2e17f74325ff8ec3e85a37fc5367c56b3343a
epicface59 <joaobatata59@gmail.com>
2024-04-18 19:25:29 +0100

    Push1

--- Veterinária/Form1.cs
+++ Veterinária/Form1.cs
@@ -24,5 +24,18 @@ namespace Veterinária
             this.Hide();
             
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.Show();
+            this.Hide();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("AAAA");
+        }
     }
 }
